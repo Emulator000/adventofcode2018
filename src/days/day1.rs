00ebbdce6969a09e2cdef907ba834de2df1e707e
Emulator000 <emulator@hotmail.it>
2018-12-05 23:38:02 +0100

    Day 1 - part 2 (optimized)

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -5,11 +5,10 @@ use inputs::{Input, day1::InputDay1};
 
 pub struct Day1 {
     input: Box<Input>,
-    numbers: HashSet<i32>,
 }
 
 impl Day for Day1 {
-    fn solve(&mut self, part: usize) -> String {
+    fn solve(&self, part: usize) -> String {
         match part {
             0 => format!("{}", self.solve1()),
             1 => format!("{}", self.solve2()),
@@ -22,7 +21,6 @@ impl Day1 {
     pub fn new() -> Self {
         Self {
             input: Box::new(InputDay1::new()),
-            numbers: HashSet::new(),
         }
     }
 
@@ -37,26 +35,22 @@ impl Day1 {
         }).sum()
     }
 
-    fn solve2(&mut self) -> i32 {
-        let input = self.input.get(0);
+    fn solve2(&self) -> i32 {
+        let mut numbers = HashSet::new();
 
         let mut sum = 0;
-        'outer: loop {
-            for num in input.split("\n") {
-                match num.parse() {
-                    Ok::<i32, _>(num) => {
+        self.input.get(0).split("\n").cycle().all(|num| {
+            match num.parse() {
+                Ok::<i32, _>(num) => {
+                    numbers.insert({
                         sum = sum + num;
 
-                        if self.numbers.contains(&sum) {
-                            break 'outer;
-                        }
-
-                        self.numbers.insert(sum);
-                    },
-                    Err(_) => {},
-                }
+                        sum
+                    })
+                },
+                Err(_) => false,
             }
-        }
+        });
 
         sum
     }
